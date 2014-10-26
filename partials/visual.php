75fcb26358170bed9acff2834e91dc6744fb4686
Ralf Hortt <me@horttcore.de>
2014-10-26 14:37:10 +0100

    Better visual output
    Fix #28

--- partials/visual.php
+++ partials/visual.php
@@ -1,12 +1,45 @@
 <?php
-if ( has_post_thumbnail() ) :
+// Get slides
+$attachments = new Attachments( 'slider' );
 
-	?>
-	<div class="post-thumbnail">
+// No output if there is no visual elemet
+if ( !has_post_thumbnail() && !$attachments->exist() )
+	return;
 
-		<?php the_post_thumbnail() ?>
+?>
 
-	</div><!-- .post-thumbnail -->
-	<?php
+<div class="visual">
 
-endif;
+	<?php if ( $attachments->exist() ) : ?>
+
+		<div class="slider">
+
+			<?php while( $attachments->get() ) : ?>
+
+				<div class="slide">
+
+					<?php echo $attachments->image( 'slider' ) ?>
+
+					<div class="slide-wrapper">
+
+						<span class="slide-content">
+
+							<?php echo apply_filters( 'the_content', $attachments->field( 'caption' ) ) ?>
+
+						</span><!-- .slide-content -->
+
+					</div><!-- .slide-wrapper -->
+
+				</div><!-- .slide -->
+
+			<?php endwhile;	?>
+
+		</div><!-- .slider -->
+
+	<?php elseif ( has_post_thumbnail() ) : ?>
+
+		<?php the_post_thumbnail( 'visual' ); ?>
+
+	<?php endif; ?>
+
+</div><!-- .visual -->

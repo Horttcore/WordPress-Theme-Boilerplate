11039518a986fd071f7f9b3909cd12ba5f15bdaf
Ralf Hortt <me@horttcore.de>
2015-01-26 16:01:05 +0100

    Remove IE8 support
    Signed-off-by: Ralf Hortt <me@horttcore.de>

--- partials/head.php
+++ partials/head.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
-<!--[if IE 8]><html id="ie8" <?php body_class(); ?> <?php language_attributes(); ?>><![endif]-->
-<!--[if !(IE 8)  ]><!--><html <?php body_class(); ?> <?php language_attributes(); ?>><!--<![endif]-->
+<html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <title><?php wp_title(''); ?></title>
-<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 <link rel="icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicon.ico" type="image/x-icon">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
 <?php wp_head(); ?>

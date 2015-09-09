33be7925be4df89d314e4f08365859ddc9914feb
Ralf Hortt <me@horttcore.de>
2015-09-09 18:18:24 +0200

    Drop favicon link in favor of WordPress support
    Signed-off-by: Ralf Hortt <me@horttcore.de>

--- partials/head.php
+++ partials/head.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<link rel="icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicon.ico" type="image/x-icon">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
 <?php wp_head(); ?>
 </head>

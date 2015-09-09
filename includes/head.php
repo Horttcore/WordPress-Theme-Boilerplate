e3ea34566bf5729b8e2ead393e8adfb662423e98
Ralf Hortt <me@horttcore.de>
2015-09-09 17:38:11 +0200

    Remove favicon
    Signed-off-by: Ralf Hortt <me@horttcore.de>

--- includes/head.php
+++ includes/head.php
@@ -1,14 +1,26 @@
 <?php
-if ( !function_exists( 'theme_favicon' ) ) :
 /**
- * Theme favicon
+ * Head
+ *
+ * This file is for functions that are hooked to wp_head
+ *
+ * @package WordPress Theme Boilerplate
+ * @author Ralf Hortt <hello@horttcore.de>
+ **/
+
+
+
+if ( !function_exists( 'theme_ajax_url' ) ) :
+/**
+ * Publish ajaxurl
  *
  * @author Ralf Hortt
  **/
-function theme_favicon()
+function theme_ajax_url()
 {
-	?><link rel="icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicon.ico" /><?php
+	?>
+	<script type="text/javascript">var ajaxurl = '<?php echo apply_filters( 'theme_ajax_url', admin_url( 'admin-ajax.php' ) ); ?>';</script>
+	<?php
 }
 endif;
-add_action( 'admin_head', 'theme_favicon' );
-add_action( 'wp_head', 'theme_favicon' );
+add_action( 'wp_head', 'theme_ajax_url' );

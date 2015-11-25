59b75b373ff94f9b9428a5ef2fa136999c86c38a
Ralf Hortt <me@horttcore.de>
2015-11-25 16:47:44 +0100

    Cleanup
    Signed-off-by: Ralf Hortt <me@horttcore.de>

--- includes/login.php
+++ includes/login.php
@@ -11,11 +11,13 @@ if ( !function_exists( 'theme_login_headerurl' ) ) :
  *
  * @param str $url URL
  * @return str URL
- * @author Ralf Hortt
+ * @author Ralf Hortt <hello@horttcore.de>
  **/
 function theme_login_headerurl( $url )
 {
+
 	return get_bloginfo( 'wpurl' );
+
 }
 endif;
 add_filter( 'login_headerurl', 'theme_login_headerurl' );
@@ -26,11 +28,13 @@ if ( !function_exists( 'theme_login_style' ) ) :
 /**
  * Theme login style
  *
- * @author Ralf Hortt
+ * @author Ralf Hortt <hello@horttcore.de>
  */
 function theme_login_style()
 {
+
 	?><link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/dest/styles/login.min.css"><?php
+
 }
 endif;
 add_action( 'login_head', 'theme_login_style' );

1fe7071c567b55ec0bd64dbc9d9b77d3304f829d
Ralf Hortt <me@horttcore.de>
2019-11-04 10:50:49 +0100

    Fix: Template output
    Signed-off-by: Ralf Hortt <me@horttcore.de>

--- resources/views/front-page.php
+++ resources/views/front-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Header
  *
@@ -17,3 +18,35 @@ get_template_part('resources/views/template-parts/head', get_post_type());
  * Header
  */
 get_template_part('resources/views/template-parts/header', get_post_type());
+
+
+/**
+ * Content
+ */
+?>
+<div class="content">
+
+    <div class="container">
+
+        <main class="main">
+
+            <?php
+            while (have_posts()) :
+                the_post();
+                get_template_part('resources/views/contents/content-archive', get_post_type());
+            endwhile;
+            ?>
+
+        </main>
+
+        <?php get_template_part('resources/views/template-parts/sidebar', get_post_type()) ?>
+
+    </div>
+
+</div>
+
+<?php
+/**
+ * Footer
+ */
+get_template_part('resources/views/footer', get_post_type());

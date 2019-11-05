803b531636eb72f1ff097475a6dc1adfbdcfe83b
Ralf Hortt <me@horttcore.de>
2019-11-05 17:12:11 +0100

    Add custom logo support
    Signed-off-by: Ralf Hortt <me@horttcore.de>

--- functions.php
+++ functions.php
@@ -56,6 +56,7 @@ add_action('after_setup_theme', function () {
     add_theme_support('post-thumbnails');
     add_theme_support('responsive-embeds');
     add_theme_support('title-tag');
+    add_theme_support('custom-logo');
     add_theme_support('align-wide');
     add_theme_support('disable-custom-colors');
     add_theme_support('disable-custom-font-sizes');

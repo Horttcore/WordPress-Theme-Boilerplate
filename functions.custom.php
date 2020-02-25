3ee1f2139adf470f5d6d8fb6fd0b6cafddf758ee
Ralf Hortt <me@horttcore.de>
2020-02-25 14:25:26 +0100

    Add inline svg attachment function

--- functions.custom.php
+++ functions.custom.php
@@ -1,6 +1,14 @@
 <?php
 /**
  * ------------------------------------------------------------------------------
- * Put theme specific functions here
+ * Helper functions
  * ------------------------------------------------------------------------------
  */
+function inlineSvg(int $attachmentId): string
+{
+    $type = get_post_mime_type($attachmentId);
+    if ( 'image/svg+xml' != $type )
+        return '';
+
+    return file_get_contents(get_attached_file($attachmentId));
+}

61da224c8eca57639270717a20510c8a06a1b4d9
Ralf Hortt <me@horttcore.de>
2020-10-01 22:58:15 +0200

    Inline SVG helpers

--- functions.helpers.php
+++ functions.helpers.php
@@ -4,3 +4,17 @@
  * Helper functions
  * ------------------------------------------------------------------------------
  */
+function inlineSvgAttachment(int $attachmentId): string
+{
+    $type = get_post_mime_type($attachmentId);
+    if ('image/svg+xml' != $type) {
+        return '';
+    }
+
+    return inlineSvg(get_attached_file($attachmentId));
+}
+
+function inlineSvg(string $file): string
+{
+    return file_get_contents($file);
+}
